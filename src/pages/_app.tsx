6e8f3b5c0aebaa7c6d7ce214985674564093c7a6
Kaique Sacchi <sacchikaique@gmail.com>
2021-03-02 16:20:25 -0300

    Added GraphQL provider.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '../services/apollo/apolloClient'
+
 import GlobalStyle from '../styles/global'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const apolloClient = useApollo(pageProps)
+
   return (
-    <>
+    <ApolloProvider client={apolloClient}>
       <Component {...pageProps} />
       <GlobalStyle />
-    </>
+    </ApolloProvider>
   )
 }
 
