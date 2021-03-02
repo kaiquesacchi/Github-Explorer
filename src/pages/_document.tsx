2451c903836239370be6f4bb816722369f839522
Kaique Sacchi <sacchikaique@gmail.com>
2021-03-02 00:01:33 -0300

    Fixed wrong Head import on _document.tsx.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,11 @@ import Document, {
   DocumentContext,
   DocumentInitialProps,
   Html,
+  Head,
   Main,
   NextScript,
 } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
-import Head from 'next/head'
 
 export default class MyDocument extends Document {
   static async getInitialProps(
