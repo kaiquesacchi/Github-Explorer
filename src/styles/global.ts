5146d14a6a964c113e719ef947f97aeaf76e5078
Kaique Sacchi <sacchikaique@gmail.com>
2021-03-10 15:50:40 -0300

    Fixed word-break rules.

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,8 +10,15 @@ body {
   font: normal 400 16px Lato, sans-serif
 }
 
-p, h1, h2, h3, h4, h5, h6 {
-  word-break: break-all;
+p, h1, h2, h3, h4, h5, h6 {  
+  -ms-word-break: break-all;
+  -ms-word-wrap: break-all;
+  -webkit-word-break: break-word;
+  -webkit-word-wrap: break-word;
+  word-break: break-word;
   word-wrap: break-word;
+  -webkit-hyphens: auto;
+  -moz-hyphens: auto;
+  hyphens: auto;
 }
 `
